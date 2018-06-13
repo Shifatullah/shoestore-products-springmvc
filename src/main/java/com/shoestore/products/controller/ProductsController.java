f9256138f95b6e5d9f41470b13d3ec61099d6d98
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2018-06-14 09:04:58 +1000

    ProductsController created

---
+++ src/main/java/com/shoestore/products/controller/ProductsController.java
@@ -0,0 +1,23 @@
+package com.shoestore.products.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class ProductsController {
+
+	@RequestMapping(value= "products", method = RequestMethod.GET)
+	public List<String> list(){
+		List<String> products = new ArrayList<String>();
+		products.add("iPhone");
+		products.add("Lenovo");
+		products.add("Ice Cream");
+		return products;
+	}
+	
+}
